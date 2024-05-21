8b9688113ef9755a95de82a3c874a80a99a007bc
Jon Leach <leachj@github.com>
2024-05-21 14:49:31 +0100

    added files and templating

---
+++ templating/blogrenderer.go
@@ -0,0 +1,54 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+
+	"github.com/gomarkdown/markdown"
+)
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
+}
+
+func (p Post) RenderedBody() template.HTML {
+	return template.HTML(markdown.ToHTML([]byte(p.Body), nil, nil))
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
---
+++ templating/go.mod
@@ -0,0 +1,8 @@
+module blogrenderer
+
+go 1.22.3
+
+require (
+	github.com/approvals/go-approval-tests v0.0.0-20240417152556-434b9105e958 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20240419095408-642f0ee99ae2 // indirect
+)
